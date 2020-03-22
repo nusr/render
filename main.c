06ef45988a798c5565ebd673a423134483590e53
steve xu <stevexugc@gmail.com>
2020-03-22 10:31:41 +0800

    fix: add Background Color

--- main.c
+++ main.c
@@ -19,47 +19,50 @@ void generateImage(const char *fileName)
     svpng(fp, width, height, rgba, 1);
     fclose(fp);
 }
-void drawDot(int x,int y,Color color){
+void drawDot(int x, int y, Color color)
+{
     int index = (x * width + y) * 4;
     rgba[index] = color.r;
-    rgba[index +1] = color.g;
-    rgba[index +2] = color.b;
-    rgba[index +3] = color.a;
+    rgba[index + 1] = color.g;
+    rgba[index + 2] = color.b;
+    rgba[index + 3] = color.a;
 }
-void drawLine(int x0,int y0,int x1,int y1,Color color){
-    float k = (y1 - y0)/(float)(x1 - x0);
+void drawLine(int x0, int y0, int x1, int y1, Color color)
+{
+    float k = (y1 - y0) / (float)(x1 - x0);
     float b = y0 * 1.0 - k * x0;
     int y;
-    for(int x = x0; x < x1;x++){
+    for (int x = x0; x < x1; x++)
+    {
         y = k * x + b;
-        drawDot(x,y,color);
+        drawDot(x, y, color);
     }
 }
-void outPutImage(const char *fileName)
+void outPutImage(const char *fileName, Color background)
 {
-    
+
     unsigned x, y;
     for (y = 0; y < width; y++)
+    {
+        for (x = 0; x < height; x++)
         {
-            for (x = 0; x < height; x++)
-        {
-            *p++ = 0;   /* R */
-            *p++ = 0;   /* G */
-            *p++ = 0;   /* B */
-            *p++ = 255; /* A */
+            *p++ = background.r; /* R */
+            *p++ = background.g; /* G */
+            *p++ = background.b; /* B */
+            *p++ = background.a; /* A */
         }
-        }
+    }
     // drawDot(0,0,RED);
     // drawDot(100,100,RED);
-    drawLine(0,0,100,200,BLUE);
-    drawLine(0,0,100,100,RED);
-    drawLine(0,0,100,50,WHITE);
+    drawLine(0, 0, 100, 200, BLUE);
+    drawLine(0, 0, 100, 100, RED);
+    drawLine(0, 0, 100, 50, WHITE);
     generateImage(fileName);
 }
 
 int main(void)
 {
     const char *output = "output.png";
-    outPutImage(output);
+    outPutImage(output, BlACK);
     return 0;
 }
